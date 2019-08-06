5e8830fa22e579f3ff8d129d8116eb3403303c73
Matt McCarthy <sk3lanimals@gmail.com>
2019-08-06 13:52:16 -0400

    feat(server/api/cart, server/api/nodemailer): adds email confirmation upon checkout

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {OrderDetail, Order} = require('../db/models')
+const mail = require('./nodemailer')
 module.exports = router
 
 router.post('/add', async (req, res, next) => {
@@ -48,6 +49,7 @@ router.put('/checkout', async (req, res, next) => {
     const order = await Order.findByPk(req.body.orderId)
     order.status = true
     order.save()
+    mail(req.user.email)
     await Order.create({
       status: false,
       userId: req.session.passport.user
